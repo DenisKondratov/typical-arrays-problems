be58a1506875c79f9b1f13d7c6f080268daaec3e
DenisKondratov <denys.kondratov@gmail.com>
2021-03-11 12:30:31 +0200

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return arguments.length != 0 && array.length != 0 ? Math.min(...array) : 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return arguments.length != 0 && array.length != 0 ? Math.max(...array) : 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let average = 0;
+  let sum = 0;
+  if (arguments.length != 0 && array.length != 0) {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+      average = sum / array.length
+    }
+    return average
+  }
+  else return 0
+
 }
